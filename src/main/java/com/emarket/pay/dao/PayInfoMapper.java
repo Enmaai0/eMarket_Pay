0cbd7cf841c98b5f8557ba3880c14f7335bf13a2
Enmaai0 <vxyu82505@gmail.com>
2024-10-25 23:49:00 +1100

    database implemented

---
+++ src/main/java/com/emarket/pay/dao/PayInfoMapper.java
@@ -0,0 +1,19 @@
+package com.emarket.pay.dao;
+
+import com.emarket.pay.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo row);
+
+    int insertSelective(PayInfo row);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo row);
+
+    int updateByPrimaryKey(PayInfo row);
+
+    PayInfo selectByOrderNo(Long orderNo);
+}
